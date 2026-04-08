ceda068eb37e53bb17da99039a022c4c0391ac5d
TanukiDong <eurondelta@gmail.com>
2026-04-08 15:27:18 +0100

    tests / nfaMerge

---
+++ tests/4_nfaMerge/test_nfaMerge_5220.py
@@ -0,0 +1,27 @@
+import pytest
+from tests.testcase.nfaMerge.testcase_nfaMerge_5220 import TESTCASE
+from tests.utils import is_dfa, has_dangling_transitions
+
+@pytest.mark.parametrize(
+    "keep_state_id, delete_state_id",
+    TESTCASE,
+    ids=[f"Keep_S{keep}-delete_S{delete}" for keep, delete in TESTCASE]
+)
+def test_nfa_merge(
+    atm_case_5220,
+    keep_state_id,
+    delete_state_id
+):
+    atm = atm_case_5220
+
+    if not atm.can_merge(keep_state_id, delete_state_id):
+        pytest.skip(f"S{keep_state_id}, S{delete_state_id} not mergeable")
+
+    ok = atm.do_single_merge(keep_state_id, delete_state_id)
+    assert ok
+
+    atm.NFA_merge(keep_state_id)
+
+    assert keep_state_id in atm.states_Dict
+    assert is_dfa(atm)
+    assert not has_dangling_transitions(atm)
---
+++ tests/testcase/nfaMerge/testcase_nfaMerge_2.py
@@ -0,0 +1,3 @@
+from itertools import combinations
+
+TESTCASE = list(combinations(range(7), 2))
